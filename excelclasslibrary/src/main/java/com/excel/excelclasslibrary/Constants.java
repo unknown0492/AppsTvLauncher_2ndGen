b4008b72975f24bc07caa249fba01e3bf25e7747
= <=>
2016-09-01 01:45:22 +0530

    modified the  comment

--- excelclasslibrary/src/main/java/com/excel/excelclasslibrary/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.excel.excelclasslibrary;
-
-public class Constants {
-
-	final public static String APP_TAG 			= "ExcelClassLibrary";
-	final public static String TAG_SEPARATOR 	= "-";
-	final public static String SDCARD_PATH		= "/mnt/sdcard/";
-	
-}
--- excelclasslibrary/src/main/java/com/excel/excelclasslibrary/UtilTethering.java
+++
@@ -1,5 +0,0 @@
-package com.excel.excelclasslibrary;
-
-public class UtilTethering {
-	
-}
