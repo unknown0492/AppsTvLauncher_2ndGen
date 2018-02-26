99fd2b288af5c93701e97864f353160c40718d6d
Sohail Shaikh <unknown0492@gmail.com>
2018-02-27 01:48:50 +0530

    v1.0.1 The Clock and the Weather has been moved to the bottom left of the launcher
    Features
    - Clock and weather has a Flipping effect when switching
    
    v1.0.2
    - Added the display of SSID and Password for Tethering at the bottom right corner of the Launcher overlapping the Collar Text
    - Fixed the flipping 90 degree bug
    
    v1.0.3
    - Added the Display of Hotel Logo
    - Created the flipping effect for Clock, Weather and Hotel Logo and also fixed the bug for flipping
    - Added the pause and resume effect for the flipping effect when launcher goes in the background
    
    v1.0.4
    - Added The Unzip of TV Channels when launcher is loaded. This happens only once after the box boot up
    
    v1.0.5
    - Added Multi-Lingual Support. Launcher Menu Items, Sub Menu items and Day/Month are set to be multi Lingual
    - Added World Clock
    - Fixed the Screensaver Bug
    - Splitted the Weather as Temperature and Remark
    - Changed the Hotspot Flipping effect to Alpha effect
    - Changed the Launcher Menu Design
    
    v1.0.7
    - Changes made for the Android 6.0
    - No killing of Apps onResume()
    - onResume() will not execute all its functions within 15 seconds of its calling
    - The flippers will be started in the onCreate() and they wont be paused and resumed
    - TV Restore will take place inside onCreate()
    - Youtube restore will support older as well as newer versions of youtube
    - Have support for PerfectTimeService date command for Android 6.0
    - Launcher items will be processed inside the Handler()

---
+++ app/src/main/java/com/excel/appstvlauncher/secondgen/WorldClockWeb.java
@@ -0,0 +1,132 @@
+package com.excel.appstvlauncher.secondgen;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.excel.configuration.ConfigurationReader;
+import com.excel.excelclasslibrary.UtilNetwork;
+import com.excel.excelclasslibrary.UtilShell;
+
+import java.io.File;
+
+public class WorldClockWeb extends Activity {
+
+    WebView wv_open_page;
+    Context context = this;
+    SharedPreferences spfs;
+    ConfigurationReader cr;
+
+    final static String TAG = "WebViewActivity";
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_web_view );
+
+        init();
+
+        Intent in = getIntent();
+        String web_view_url = in.getStringExtra( "web_view_url" );
+        String params = in.getStringExtra( "params" );
+
+        String URL = cr.getProtocol() + "://" + cr.getCmsIp() + File.separator + cr.getCmsSubDirectory() + File.separator + "webservice.php?";
+        // String url_params = String.format( "what_do_you_want=%s&web_view_url=%s&", "cms_template", web_view_url );
+        String url_params = String.format( "what_do_you_want=%s&url_type=%s&", "url_forward", web_view_url );
+        String params_arr[] = params.split( "," );
+        for( int i = 0 ; i < params_arr.length ; i++ ){
+
+            if( params_arr[ i ].equals( "mac_address" ) ){
+                url_params += "mac_address=" + UtilNetwork.getMacAddress( context ) + "&";
+            }
+            else if( params_arr[ i ].equals( "language_code" ) ){
+                String language_code = UtilShell.executeShellCommandWithOp( "getprop language_code" ).trim();
+                url_params += "language_code=" + language_code + "&";
+            }
+
+        }
+        url_params = url_params.substring( 0, url_params.length() - 1 );
+        URL = URL + url_params;
+        Log.i( TAG, URL );
+
+        wv_open_page.loadUrl( URL );
+
+        setContentView( wv_open_page );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        wv_open_page.clearCache( true );
+
+        context.deleteDatabase( "webview.db" );
+        context.deleteDatabase( "webviewCache.db" );
+
+        File dir = getCacheDir();
+
+        if ( dir != null && dir.isDirectory() ){
+            try{
+                File[] children = dir.listFiles();
+                if ( children.length > 0 ){
+                    for ( int i = 0; i < children.length; i++ ){
+                        File[] temp = children[ i ].listFiles();
+                        for ( int x = 0; x < temp.length; x++ ){
+                            temp[ x ].delete();
+                        }
+                    }
+                }
+            }
+            catch ( Exception e ){
+                Log.e( TAG, "failed cache clean" );
+            }
+        }
+        Log.i( TAG, "onDestroy()" );
+    }
+
+    private void init(){
+        wv_open_page = new WebView( this );
+        wv_open_page.getSettings().setJavaScriptEnabled( true );
+        wv_open_page.getSettings().setAppCacheEnabled( false );
+
+        cr = ConfigurationReader.getInstance();
+
+        final Activity activity = this;
+
+        wv_open_page.setWebViewClient( new WebViewClient() {
+
+            ProgressDialog p;
+
+            public void onReceivedError( WebView view, int errorCode, String description, String failingUrl ) {
+                wv_open_page.loadUrl( "file:///android_asset/maintenance/maintenance.html" );
+            }
+
+            @Override
+            public void onPageStarted( WebView view, String url, Bitmap favicon ) {
+                super.onPageStarted( view, url, favicon );
+            }
+
+            @Override
+            public void onPageFinished( WebView view, String url ) {
+                super.onPageFinished( view, url );
+            }
+
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i( TAG, "onPause()" );
+
+        finish();
+    }
+}
